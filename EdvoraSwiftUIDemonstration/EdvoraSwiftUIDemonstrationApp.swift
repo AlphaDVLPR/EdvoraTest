d7c0217fc71d448761cd15e15dfc97665e45df16
Jesse Rae <51697381+AlphaDVLPR@users.noreply.github.com>
2022-01-10 20:00:03 -0700

    Initial Commit

---
+++ EdvoraSwiftUIDemonstration/EdvoraSwiftUIDemonstrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraSwiftUIDemonstrationApp.swift
+//  EdvoraSwiftUIDemonstration
+//
+//  Created by Jesse Cacanindin on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraSwiftUIDemonstrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
